53677534c790d746300d48c9033852d363e5aa0e
alaaelfeshawy <alaaelfeshawy85@gmail.com>
2025-01-11 15:38:40 +0200

    revert commit

--- home/src/main/java/example/alaa/home/ui/component/tabs/singletrip/SingleTripViewModel.kt
+++ home/src/main/java/example/alaa/home/ui/component/tabs/singletrip/SingleTripViewModel.kt
@@ -13,6 +13,7 @@ import example.alaa.home.ui.model.PassengerUiModel
 import example.alaa.home.ui.model.SearchModel
 import example.alaa.home.ui.model.TripType
 import javax.inject.Inject
+
 @HiltViewModel
 class SingleTripViewModel @Inject constructor(): BaseViewModel<
         SingleTripIntent, TabState, TabEvent>() {
